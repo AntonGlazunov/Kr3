c04539207b35dee39b0d16d71050540a48cd21fb
Anton Glazunov <tagl91@yandex.ru>
2024-03-16 18:47:58 +0300

    add class BankOperations & exemplar send_format_date

--- func/class.py
+++ func/class.py
@@ -1,3 +1,16 @@
 class BankOperations:
 
-    def __init__():
+    def __init__(self, id_operations, state, date, amount, currency_code, description, from_, to):
+        self.id_operations = id_operations
+        self.state = state
+        self.date = date
+        self.amount = amount
+        self.currency_code = currency_code
+        self.description = description
+        self.from_ = from_
+        self.to = to
+
+    def send_format_date(self):
+        """Возвращает дату в формате ДД.ММ.ГГГГ
+        """
+        return ".".join(self.date.split("T")[0].split("-")[::-1])
