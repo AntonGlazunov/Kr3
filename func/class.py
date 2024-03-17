492ccca84e33be7dd728c871cf72a4561e7b8bf4
Anton Glazunov <tagl91@yandex.ru>
2024-03-17 13:16:58 +0300

    add exemplar send_from_

--- func/class.py
+++ func/class.py
@@ -11,6 +11,18 @@ class BankOperations:
         self.to = to
 
     def send_format_date(self):
-        """Возвращает дату в формате ДД.ММ.ГГГГ
+        """Возвращает дату в формате str(ДД.ММ.ГГГГ)
         """
         return ".".join(self.date.split("T")[0].split("-")[::-1])
+
+    def send_from_(self):
+        """Возвращает описание отправителя перевода
+        в формате (Visa Platinum 0000 00** **** 0000)
+        :param payment_system: название платежной системы
+        :param number_card: Номер карты
+        :param encoded_number: Зашифрованный номер карты
+        """
+        payment_system = self.from_.split(' ')[0]
+        number_card = self.from_.split(' ')[1]
+        encoded_number = number_card.replace(number_card[6:12], "******", 1)
+        return " ".join([payment_system, encoded_number[0:4], encoded_number[4:8], encoded_number[8:12], encoded_number[12:16]])
