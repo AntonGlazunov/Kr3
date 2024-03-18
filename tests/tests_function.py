fd2387276e18935e2b2cdeb03d7d743ee0f9609c
Anton Glazunov <tagl91@yandex.ru>
2024-03-18 23:56:38 +0300

    Добавил тесты

---
+++ tests/tests_function.py
@@ -0,0 +1,60 @@
+import pytest
+
+import func.functions
+
+
+def test_load_operations_list():
+    assert func.functions.load_operations_list("..", "text.json") == [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            }
+        }
+    ]
+
+
+def test_rearrangement():
+    assert func.functions.rearrangement([5, 4, 3, 2, 1], 6) == "Не верное число"
+
+
+
+def test_object_generator():
+    assert func.functions.object_generator([
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {
+                "amount": "31957.58",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589"
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {
+                "amount": "8221.37",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "to": "Счет 35383033474447895560"
+        }, {}])[0].format_date() == "26.08.2019"
+
+
